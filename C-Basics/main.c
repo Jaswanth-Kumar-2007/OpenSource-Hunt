60bb5586cf4abea516268acf674cbd1e2d9ef7a4
karanitkrm <karanitkrm@gmail.com>
2026-08-21 08:12:45 +0530

    Fix missing semicolon in main.c

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -12,7 +12,7 @@ int main()
     printf("Subtraction: %d\n", subtract(a, b));
     printf("Multiplication: %d\n", multiply(a, b));
 
-    printf("Division: %d\n", divide(a, b)
+    printf("Division: %d\n", divide(a, b));
 
     return 0;
 }
