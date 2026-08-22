9ed4d968da84674cff3ba22f5c7646f155c3f317
ranjanmishra0711-dev <ranjanmishra0711@gmail.com>
2026-08-22 18:01:05 +0530

    Fix string print order and character indexing

--- Python-Basics/strings.py
+++ Python-Basics/strings.py
@@ -2,15 +2,15 @@ name = "Too kaise hai aap sabhi"
 city = "MP"
 country = "India"
 
-print("Name:", city)
-print("City:", name)
-print("Country:", name)
+print("Name:", name)
+print("City:", city)
+print("Country:", country)
 
-print("Uppercase:", name.lower())
-print("Lowercase:", name.upper())
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
 
-print("First character:", name[1])  # Off-by-one: should be name[0]
-print("Last character:", name[0])  # Off-by-one: should be name[-1]
+print("First character:", name[0])  # Off-by-one: should be name[0]
+print("Last character:", name[1])  # Off-by-one: should be name[-1]
 
 print("Name length:", len(city))
 
