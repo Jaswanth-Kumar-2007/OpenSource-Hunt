f25f193130cbfb2aa9ee6ec67ca6b1fc5cf1ebef
Apoorva Chauhan <apoorvachauhan10@gmail.com>
2026-08-22 18:13:05 +0530

    Fix conditionals for age, marks, and temperature

--- Python-Basics/conditions.py
+++ Python-Basics/conditions.py
@@ -3,18 +3,18 @@ marks = 85
 temperature = 30
 
 if age >= 18:
-    print("You are a child")
-else:
     print("You are an adult")
+else:
+    print("You are a child")
 
 if marks >= 90:
-    print("Grade B")
-elif marks >= 75:
     print("Grade A")
+elif marks >= 75:
+    print("Grade B")
 else:
     print("Grade C")
 
-if temperature > 30:  # Changed threshold but message still makes no sense
-    print("It is cold")
-else:
+if temperature >= 30:  # Changed threshold but message still makes no sense
     print("It is hot")
+else:
+    print("It is cold")
