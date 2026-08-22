bf8b45060110059159c50ea4969c71510e118df2
Apoorva Chauhan <apoorvachauhan10@gmail.com>
2026-08-22 18:15:00 +0530

    Fix loop conditions and print statements in loops.py

--- Python-Basics/loops.py
+++ Python-Basics/loops.py
@@ -3,7 +3,7 @@ for i in range(1, 6):
 
 for i in range(1, 11):
     if i % 2 == 1:
-        print("Even number:", i)
+        print("Odd number:", i)
 
-for i in range(5, 0):  # Missing stop value, will count forever or error
+for i in range(5, 0, -1):  # Missing stop value, will count forever or error
     print("Countdown:", i)
