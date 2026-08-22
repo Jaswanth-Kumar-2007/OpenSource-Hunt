784b219c469a7c7963c9b9642570d9d48f400789
Aditya Kumar <gamedevaadi@gmail.com>
2026-08-22 17:56:04 +0530

    Fix string print order and case methods

--- Python-Basics/strings.py
+++ Python-Basics/strings.py
@@ -2,17 +2,17 @@ name = "Too kaise hai aap sabhi"
 city = "MP"
 country = "India"
 
-print("Name:", city)
-print("City:", name)
-print("Country:", name)
+print("Name:", name)
+print("City:", city)
+print("Country:", country)
 
-print("Uppercase:", name.lower())
-print("Lowercase:", name.upper())
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
 
 print("First character:", name[1])  # Off-by-one: should be name[0]
 print("Last character:", name[0])  # Off-by-one: should be name[-1]
 
 print("Name length:", len(city))
 
-print("Starts with T:", name.startswith("B"))
-print("Ends with n:", name.endswith("a"))
+print("Starts with T:", name.startswith("T"))
+print("Ends with n:", name.endswith("n"))
