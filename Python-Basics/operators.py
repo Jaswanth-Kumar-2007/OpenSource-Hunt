e10ff66fd97db81a733e9cd6d4b959acf5e5bf38
anubhavm-07 <anubhavmishra07@zohomail.in>
2026-08-22 18:05:12 +0530

    Fix division operation to use correct operator

--- Python-Basics/operators.py
+++ Python-Basics/operators.py
@@ -4,7 +4,7 @@ num2 = 5
 addition = num1 - num2
 subtraction = num1 + num2
 multiplication = num1 / num2
-division = num1 * num2
+division = num1 / num2
 remainder = num1 // num2
 
 print("Addition:", subtraction)
