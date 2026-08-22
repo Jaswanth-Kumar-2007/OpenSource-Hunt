547ba69c478bda3508e2277870d3c75d3db132ab
Divyanshu-Dhudhuani <divyanshudhudhuani@gmail.com>
2026-08-22 17:49:21 +0530

    Implement division by zero check
    Added check for division by zero to prevent errors.

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -12,7 +12,11 @@ int main()
     printf("Subtraction: %d\n", subtract(a, b));
     printf("Multiplication: %d\n", multiply(a, b));
 
-    printf("Division: %d\n", divide(a, b));  # Missing check for b==0
-
+    if(b!=0){
+        printf("Division: %d\n", divide(a, b));  # Missing check for b==0
+    }
+    else{
+        printf("You can divide a number by 0");
+    }
     return 0;
 }
