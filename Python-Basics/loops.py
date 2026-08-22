e5eb26dea4e3f0d05bac4b97fc8a79ce3488116f
Saumitra-53 <saumitra.7485@gmail.com>
2026-08-22 18:05:45 +0530

    Update loops.py

--- Python-Basics/loops.py
+++ Python-Basics/loops.py
@@ -5,5 +5,5 @@ for i in range(1, 11):
     if i % 2 == 1:
         print("Even number:", i)
 
-for i in range(5, 0):  # Missing stop value, will count forever or error
+for i in range(5, 0, -1):  # Missing stop value, will count forever or error
     print("Countdown:", i)
