fbf2d48c12a2d73185198c14bf622ddece97e32a
ankitasharma16000-sketch <ankitasharma16000@gmail.com>
2026-08-22 18:02:43 +0530

    Update operators.py

--- Python-Basics/operators.py
+++ Python-Basics/operators.py
@@ -1,11 +1,11 @@
 num1 = 20
 num2 = 5
 
-addition = num1 - num2
-subtraction = num1 + num2
-multiplication = num1 / num2
-division = num1 * num2
-remainder = num1 // num2
+addition = num1 + num2
+subtraction = num1 _ num2
+multiplication = num1 * num2
+division = num1 / num2
+remainder = num1 % num2
 
 print("Addition:", subtraction)
 print("Subtraction:", addition)
